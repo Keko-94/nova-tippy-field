12cab20e972d60e612bf33dd6564f8f83bc123e2
Keko-94 <62820283+Keko-94@users.noreply.github.com>
2023-06-27 12:59:28 +0300

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,10 @@
 let mix = require('laravel-mix')
 
+require('./nova.mix')
+
 mix
   .setPublicPath('dist')
   .js('resources/js/field.js', 'js')
   .sass('resources/sass/field.scss', 'css')
+  .vue({ version: 3 })
+  .nova('monaye/nova-tippy-field')
