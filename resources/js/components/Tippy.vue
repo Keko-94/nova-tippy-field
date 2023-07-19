eb610c0f93eede011df6cd048258e2472d661e0a
Keko-94 <62820283+Keko-94@users.noreply.github.com>
2023-07-19 15:08:07 +0200

    Update Tippy.vue

--- resources/js/components/Tippy.vue
+++ resources/js/components/Tippy.vue
@@ -21,8 +21,43 @@ export default {
 
     mounted() {
         this.$nextTick(() => {
-            if(this.tipContent) {
+            if (this.tipContent !== this.text) {
+                tippy('#nova-simple-tooltip' + this.id, {
+                    content: this.tipContent,
+                    placement: this.placement,
+                    ...this.tippyOptions
+                });
+            }
+        });
 
+    }
+
+}
+</script>
+<template>
+    <span v-if="tipContent"
+          :id="'nova-simple-tooltip' + id "
+          class="nova-simple-tooltip cursor-pointer items-center flex justify-center text-90 hover:text-primary"
+          ref="tooltip">
+        <span class="whitespace-no-wrap" v-if="text && !iconUrl" v-html="text"></span>
+        <span class="mr-2" v-if=" text && iconUrl && iconPosition === 'right'" v-html="text"></span>
+        <img v-if="iconUrl" :width="iconSize" :src="iconUrl" alt="Image description." />
+        <span class="inline-flex text-70" v-if="iconPath" v-html="iconPath"></span>
+        <span class="ml-2 " v-if=" text && iconUrl && iconPosition === 'left'" v-html="text"></span>
+    </span>
+</template>
+
+<script>
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
+export default {
+
+    props: ['value', 'iconPath', 'iconUrl', 'iconSize', 'text', 'iconPosition', 'tipContent', 'id', 'placement', 'tippyOptions'],
+
+    mounted() {
+        this.$nextTick(() => {
+            if (this.tipContent !== this.text) {
                 tippy('#nova-simple-tooltip' + this.id, {
                     content: this.tipContent,
                     placement: this.placement,
